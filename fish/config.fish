795a936211d072a50165c19b91f9609cda63fa9c
Tadas Sce <tadas@sce.lt>
2014-07-08 14:13:44 +0300

    Add personal bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,4 @@ set PATH /usr/local/sbin $PATH
 set PATH /usr/local/share/npm/bin $PATH
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
-set PATH $HOME/bin $PATH
+set PATH $HOME/Dropbox/Dotfiles/bin $PATH
