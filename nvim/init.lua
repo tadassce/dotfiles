02de56b66aef71685ea1d77daab3321199fb20fb
Tadas Scerbinskas <tadas@sce.lt>
2026-08-20 21:21:12 +0100

    Move dotfiles repository under src

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
-require('user.settings')  -- ~/.dotfiles/nvim/lua/user/settings.lua
-require('user.plugins')   -- ~/.dotfiles/nvim/lua/user/plugins.lua
-require('user.functions') -- ~/.dotfiles/nvim/lua/user/functions.lua
-require('user.keymaps')   -- ~/.dotfiles/nvim/lua/user/keymaps.lua
-require('user.final')     -- ~/.dotfiles/nvim/lua/user/final.lua
+require('user.settings')  -- ~/src/dotfiles/nvim/lua/user/settings.lua
+require('user.plugins')   -- ~/src/dotfiles/nvim/lua/user/plugins.lua
+require('user.functions') -- ~/src/dotfiles/nvim/lua/user/functions.lua
+require('user.keymaps')   -- ~/src/dotfiles/nvim/lua/user/keymaps.lua
+require('user.final')     -- ~/src/dotfiles/nvim/lua/user/final.lua
